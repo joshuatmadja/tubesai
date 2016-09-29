13a6274c07559f6140bf2a72553b7efd36933270
hasnank <hasnurk@gmail.com>
2016-09-29 12:04:09 +0700

    update tapi belum dites hehe

--- Classes/Assign.py
+++ Classes/Assign.py
@@ -1,14 +1,22 @@
 from .Jadwal import Jadwal
 
 class Assign:
-	#belom aktifin jadwal
-	#belum nyari ruang di daftar_ruangan
-	#belum aktifin daftar_matkul_time
+	daftar_matkul_time = []
+	
+	def __init__(self, tc):
+		self.jadwal = Jadwal(tc)
+	
+	def nama_to_ruang(nama):
+		for ruang in self.jadwal.daftar_ruangan:
+			if ruang.nama == nama:
+				return ruang
+				break
 		
 	def cariruang(matkul):
+		temp_matkul_time = MatKulOnlyTime
 		bol = False
 		if matkul.ruangan != '-':
-			ruang = matkul.ruangan
+			ruang = nama_to_ruang(matkul.ruangan)
 			for hari in matkul.hari:
 				if bol:
 					break
@@ -16,11 +24,12 @@ class Assign:
 					if hari == haris:
 						if matkul.jam_awal >= ruang.jam_awal:
 							if matkul.jam_awal + matkul.sks <= ruang.jam_akhir:
-								matkul.setTime(ruang, matkul.jam_awal, hari, matkul.sks)
+								temp_matkul_time.setTime(ruang, matkul.jam_awal, hari, matkul.sks)
+								self.daftar_matkul_time.append(temp_matkul_time)
 								bol = True
 								break	
 		else:
-			for ruang in jadwal.daftar_ruangan:
+			for ruang in self.jadwal.daftar_ruangan:
 				if bol:
 					break				
 				for hari in matkul.hari:
@@ -30,10 +39,12 @@ class Assign:
 						if hari == haris:
 							if matkul.jam_awal >= ruang.jam_awal:
 								if matkul.jam_awal + matkul.sks <= ruang.jam_akhir:
-									matkul.setTime(ruang, matkul.jam_awal, hari, matkul.sks)
+									temp_matkul_time.setTime(ruang, matkul.jam_awal, hari, matkul.sks)
+									self.daftar_matkul_time.append(temp_matkul_time)
 									bol = True
 									break
 				
-	def masukkan(matkuls):
-		for matkul in matkuls:			
+	def masukkan():
+		for matkul in self.jadwal.daftar_mata_kuliah:			
 			cariruang(matkul)
+	
