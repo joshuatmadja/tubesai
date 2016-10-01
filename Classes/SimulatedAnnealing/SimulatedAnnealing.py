ef16ec31b7aa287c96b86a7f4448ab7754fb0e31
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2016-10-01 16:48:24 +0700

    Cleaning SimulatedAnnealing

--- Classes/SimulatedAnnealing/SimulatedAnnealing.py
+++ Classes/SimulatedAnnealing/SimulatedAnnealing.py
@@ -2,18 +2,18 @@ import copy
 import math
 import random
 from ..Matriks import Matriks
-
 class SimulatedAnnealing:
 
 	# Acceptance function, energy represent conflict_count
-	def acceptance_function(self, current_energy, new_energy, temperature):
+	@classmethod
+	def acceptance_function(cls, current_energy, new_energy, temperature):
 		if (new_energy <= current_energy):
 			return 1
 		else:
 			return math.exp((current_energy - new_energy) / temperature)
 
-	def calculate(self)
-
+	@classmethod
+	def calculate(cls):
 		# Looping - simulated annealing main point
 		while temperature > 1 and least_conflict > 0:
 			# Create new Matriks for swapping & conflict_count comparison
@@ -22,17 +22,19 @@ class SimulatedAnnealing:
 			new_conflict = new_solution.conflict_count()
 
 			# Decide & keep best solution
-			if (self.acceptance_function(least_conflict, new_conflict, temperature) >= random.randrange(0, 1)):
+			if (cls.acceptance_function(least_conflict, new_conflict, temperature) >= random.randrange(0, 1)):
 				current_solution = new_solution
 				least_conflict = new_conflict
 
 			# Cooling
-			temperature *= 1 - cooling_rate
+			temperature *= (1 - cooling_rate)
 
-	def __init__(self, temperature, cooling_rate, current_solution, least_conflict):
+	@classmethod
+	def __init__(cls):
 		# Initiate temperature & cooling rate
-		self.temperature = 1000
-		self.cooling_rate = 0.03
+		cls.temperature = 1000
+		cls.cooling_rate = 0.03
+
 		# Initiate Matriks - complete assignment
-		self.current_solution = Matriks()
-		self.least_conflict = current_solution.conflict_count()
+		cls.current_solution = Matriks()
+		cls.least_conflict = current_solution.conflict_count()
