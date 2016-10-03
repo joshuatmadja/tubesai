ea5ce8946df755c541aa57ec3943f922bc464001
hasnank <hasnurk@gmail.com>
2016-10-03 16:24:33 +0700

    kalem dikit lagi

--- Classes/Assign.py
+++ Classes/Assign.py
@@ -1,5 +1,6 @@
 from .Jadwal import Jadwal
 from .MatKulOnlyTime import MatKulOnlyTime
+from .Matriks import Matriks
 class Assign:
 	daftar_matkul_time = []
 
@@ -12,9 +13,28 @@ class Assign:
 		return None
 
 	@classmethod
+	def hitung_kosong(ruang, jam_awal, jam_akhir):
+		selisih = jam_akhir - jam_awal
+		hitung = 0
+		for i in range(selisih):
+			if not self.matriks[ruang][jam_awal+i]:
+				hitung += 1
+		return hitung
+		
+	@classmethod
+	def min_kosong(daftar):
+		minim = 0
+		for i in range(len(daftar)):
+			if daftar[i] < daftar[minim]:
+				minim = i
+		return minim
+
+	@classmethod
 	def cariruang(cls, matkul):
+		
 		temp_matkul_time = MatKulOnlyTime()
-		bol = False
+		daftar_temp = []
+		daftar_kosong = []
 		if matkul.ruangan != '-':
 			idx = cls.nama_to_ruang_idx(matkul.ruangan)
 			ruang = Jadwal.daftar_ruangan[idx]
@@ -23,12 +43,13 @@ class Assign:
 					if hari == haris:
 						if max(matkul.jam_awal, ruang.jam_awal) + matkul.sks <= min(matkul.jam_akhir, ruang.jam_akhir):
 							temp_matkul_time.setTime(idx, max(matkul.jam_awal, ruang.jam_awal), hari, matkul.sks)
-							cls.daftar_matkul_time.append(temp_matkul_time)
-							bol = True
-							break
-
-				if bol:
-					break
+							kosong = self.hitung_kosong(temp_matkul_time.r_selected, temp_matkul_time.j_selected, min(matkul.jam_akhir, ruang.jam_akhir))
+							a = min(kosong, matkul.sks)
+							b = max(kosong, matkul.sks)
+							kosong = b - a
+							daftar_kosong.append(kosong)
+							daftar_temp.append(temp_matkul_time)
+						
 		else:
 			n = len(Jadwal.daftar_ruangan)
 			for idx in range(n):
@@ -38,18 +59,26 @@ class Assign:
 						if hari == haris:
 							if max(matkul.jam_awal, ruang.jam_awal) + matkul.sks <= min(matkul.jam_akhir, ruang.jam_akhir):
 								temp_matkul_time.setTime(idx, max(matkul.jam_awal, ruang.jam_awal), hari, matkul.sks)
-								cls.daftar_matkul_time.append(temp_matkul_time)
-								bol = True
-								break
-						if bol:
-							break
-					if bol:
-						break
-				if bol:
-					break
-
+								kosong = self.hitung_kosong(temp_matkul_time.r_selected, temp_matkul_time.j_selected, min(matkul.jam_akhir, ruang.jam_akhir))
+								a = min(kosong, matkul.sks)
+								b = max(kosong, matkul.sks)
+								kosong = b - a
+								kosong = kosong - matkul.sks
+								daftar_kosong.append(kosong)
+								daftar_temp.append(temp_matkul_time)
+						
+		minim = self.min_kosong(kosong)
+		temp_matkul_time_final = temp_matkul_time[minim]
+		cls.daftar_matkul_time.append(temp_matkul_time_final)
+		
+		c = (temp_matkul_time_final.h_selected - 1) * 24 + temp_matkul_time_final.j_selected
+		for i in range(temp_matkul_time_final.sks):
+			self.matriks(temp_matkul_time_final.r_selected, c+i)
+							
+								
 	@classmethod
 	def __init__(cls):
+		self.matriks = Matriks()
 		cls.daftar_matkul_time = []
 		for matkul in Jadwal.daftar_mata_kuliah:
 			cls.cariruang(matkul)
